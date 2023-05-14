34d925329ea2dab7142c229431e9a8776a020290
dev-softenin <alifahmed.47@gmail.com>
2023-05-14 16:33:33 +0600

    init: project initialized

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import '@/styles/globals.css';
+
+export const metadata = {
+  title: 'Promptopia',
+  description: 'Discover and Share AI Prompts',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient'></div>
+        </div>
+
+        <main className='app'>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1>Homepage</h1>
+    </section>
+  );
+};
+
+export default Home;
